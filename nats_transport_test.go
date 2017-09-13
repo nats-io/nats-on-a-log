4247c3f57df6196df23583b264fa3c9173864cef
Tyler Treat <ttreat31@gmail.com>
2017-09-13 14:15:09 -0500

    Add unit tests

---
+++ nats_transport_test.go
@@ -0,0 +1,554 @@
+package natslog
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"reflect"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/raft"
+	"github.com/nats-io/gnatsd/server"
+	gnatsd "github.com/nats-io/gnatsd/test"
+	"github.com/nats-io/go-nats"
+)
+
+// This can be used as the destination for a logger and it'll
+// map them into calls to testing.T.Log, so that you only see
+// the logging for failed tests.
+type testLoggerAdapter struct {
+	t      *testing.T
+	prefix string
+}
+
+func (a *testLoggerAdapter) Write(d []byte) (int, error) {
+	if d[len(d)-1] == '\n' {
+		d = d[:len(d)-1]
+	}
+	if a.prefix != "" {
+		l := a.prefix + ": " + string(d)
+		a.t.Log(l)
+		return len(l), nil
+	}
+
+	a.t.Log(string(d))
+	return len(d), nil
+}
+
+func newTestLogger(t *testing.T) *log.Logger {
+	return log.New(&testLoggerAdapter{t: t}, "", log.Lmicroseconds)
+}
+
+// So that we can pass tests and benchmarks...
+type tLogger interface {
+	Fatalf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+}
+
+// RunDefaultServer will run a server on the default port.
+func RunDefaultServer() *server.Server {
+	return RunServerOnPort(nats.DefaultPort)
+}
+
+// RunServerOnPort will run a server on the given port.
+func RunServerOnPort(port int) *server.Server {
+	opts := gnatsd.DefaultTestOptions
+	opts.Port = port
+	return RunServerWithOptions(opts)
+}
+
+// RunServerWithOptions will run a server with the given options.
+func RunServerWithOptions(opts server.Options) *server.Server {
+	return gnatsd.RunServer(&opts)
+}
+
+func NewDefaultConnection(t tLogger) *nats.Conn {
+	return NewConnection(t, nats.DefaultPort)
+}
+
+// NewConnection forms connection on a given port.
+func NewConnection(t tLogger, port int) *nats.Conn {
+	url := fmt.Sprintf("nats://localhost:%d", port)
+	nc, err := nats.Connect(url)
+	if err != nil {
+		t.Fatalf("Failed to create default connection: %v\n", err)
+		return nil
+	}
+	return nc
+}
+
+func TestNATSTransportStartStop(t *testing.T) {
+	s := RunDefaultServer()
+	defer s.Shutdown()
+	nc := NewDefaultConnection(t)
+	defer nc.Close()
+
+	trans, err := NewNATSTransportWithLogger("a", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	trans.Close()
+}
+
+func TestNATSTransportHeartbeatFastPath(t *testing.T) {
+	s := RunDefaultServer()
+	defer s.Shutdown()
+	nc := NewDefaultConnection(t)
+	defer nc.Close()
+
+	// Transport 1 is consumer
+	trans1, err := NewNATSTransportWithLogger("a", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans1.Close()
+
+	// Make the RPC request
+	args := raft.AppendEntriesRequest{
+		Term:   10,
+		Leader: []byte("cartman"),
+	}
+	resp := raft.AppendEntriesResponse{
+		Term:    4,
+		LastLog: 90,
+		Success: true,
+	}
+
+	invoked := false
+	fastpath := func(rpc raft.RPC) {
+		// Verify the command
+		req := rpc.Command.(*raft.AppendEntriesRequest)
+		if !reflect.DeepEqual(req, &args) {
+			t.Fatalf("command mismatch: %#v %#v", *req, args)
+		}
+
+		rpc.Respond(&resp, nil)
+		invoked = true
+	}
+	trans1.SetHeartbeatHandler(fastpath)
+
+	// Transport 2 makes outbound request
+	trans2, err := NewNATSTransportWithLogger("b", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans2.Close()
+
+	var out raft.AppendEntriesResponse
+	if err := trans2.AppendEntries(trans1.LocalAddr(), &args, &out); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Verify the response
+	if !reflect.DeepEqual(resp, out) {
+		t.Fatalf("command mismatch: %#v %#v", resp, out)
+	}
+
+	// Ensure fast-path is used
+	if !invoked {
+		t.Fatalf("fast-path not used")
+	}
+}
+
+func TestNATSTransportAppendEntries(t *testing.T) {
+	s := RunDefaultServer()
+	defer s.Shutdown()
+	nc := NewDefaultConnection(t)
+	defer nc.Close()
+
+	// Transport 1 is consumer
+	trans1, err := NewNATSTransportWithLogger("a", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans1.Close()
+	rpcCh := trans1.Consumer()
+
+	// Make the RPC request
+	args := raft.AppendEntriesRequest{
+		Term:         10,
+		Leader:       []byte("cartman"),
+		PrevLogEntry: 100,
+		PrevLogTerm:  4,
+		Entries: []*raft.Log{
+			&raft.Log{
+				Index: 101,
+				Term:  4,
+				Type:  raft.LogNoop,
+			},
+		},
+		LeaderCommitIndex: 90,
+	}
+	resp := raft.AppendEntriesResponse{
+		Term:    4,
+		LastLog: 90,
+		Success: true,
+	}
+
+	// Listen for a request
+	go func() {
+		select {
+		case rpc := <-rpcCh:
+			// Verify the command
+			req := rpc.Command.(*raft.AppendEntriesRequest)
+			if !reflect.DeepEqual(req, &args) {
+				t.Fatalf("command mismatch: %#v %#v", *req, args)
+			}
+
+			rpc.Respond(&resp, nil)
+
+		case <-time.After(200 * time.Millisecond):
+			t.Fatalf("timeout")
+		}
+	}()
+
+	// Transport 2 makes outbound request
+	trans2, err := NewNATSTransportWithLogger("b", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans2.Close()
+
+	var out raft.AppendEntriesResponse
+	if err := trans2.AppendEntries(trans1.LocalAddr(), &args, &out); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Verify the response
+	if !reflect.DeepEqual(resp, out) {
+		t.Fatalf("command mismatch: %#v %#v", resp, out)
+	}
+}
+
+func TestNATSTransportAppendEntriesPipeline(t *testing.T) {
+	s := RunDefaultServer()
+	defer s.Shutdown()
+	nc := NewDefaultConnection(t)
+	defer nc.Close()
+
+	// Transport 1 is consumer
+	trans1, err := NewNATSTransportWithLogger("a", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans1.Close()
+	rpcCh := trans1.Consumer()
+
+	// Make the RPC request
+	args := raft.AppendEntriesRequest{
+		Term:         10,
+		Leader:       []byte("cartman"),
+		PrevLogEntry: 100,
+		PrevLogTerm:  4,
+		Entries: []*raft.Log{
+			&raft.Log{
+				Index: 101,
+				Term:  4,
+				Type:  raft.LogNoop,
+			},
+		},
+		LeaderCommitIndex: 90,
+	}
+	resp := raft.AppendEntriesResponse{
+		Term:    4,
+		LastLog: 90,
+		Success: true,
+	}
+
+	// Listen for a request
+	go func() {
+		for i := 0; i < 10; i++ {
+			select {
+			case rpc := <-rpcCh:
+				// Verify the command
+				req := rpc.Command.(*raft.AppendEntriesRequest)
+				if !reflect.DeepEqual(req, &args) {
+					t.Fatalf("command mismatch: %#v %#v", *req, args)
+				}
+				rpc.Respond(&resp, nil)
+
+			case <-time.After(200 * time.Millisecond):
+				t.Fatalf("timeout")
+			}
+		}
+	}()
+
+	// Transport 2 makes outbound request
+	trans2, err := NewNATSTransportWithLogger("b", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans2.Close()
+
+	pipeline, err := trans2.AppendEntriesPipeline(trans1.LocalAddr())
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer pipeline.Close()
+	for i := 0; i < 10; i++ {
+		out := new(raft.AppendEntriesResponse)
+		if _, err := pipeline.AppendEntries(&args, out); err != nil {
+			t.Fatalf("err: %v", err)
+		}
+	}
+
+	respCh := pipeline.Consumer()
+	for i := 0; i < 10; i++ {
+		select {
+		case ready := <-respCh:
+			// Verify the response
+			if !reflect.DeepEqual(&resp, ready.Response()) {
+				t.Fatalf("command mismatch: %#v %#v", &resp, ready.Response())
+			}
+		case <-time.After(200 * time.Millisecond):
+			t.Fatalf("timeout")
+		}
+	}
+}
+
+func TestNATSTransportRequestVote(t *testing.T) {
+	s := RunDefaultServer()
+	defer s.Shutdown()
+	nc := NewDefaultConnection(t)
+	defer nc.Close()
+
+	// Transport 1 is consumer
+	trans1, err := NewNATSTransportWithLogger("a", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans1.Close()
+	rpcCh := trans1.Consumer()
+
+	// Make the RPC request
+	args := raft.RequestVoteRequest{
+		Term:         20,
+		Candidate:    []byte("butters"),
+		LastLogIndex: 100,
+		LastLogTerm:  19,
+	}
+	resp := raft.RequestVoteResponse{
+		Term:    100,
+		Peers:   []byte("blah"),
+		Granted: false,
+	}
+
+	// Listen for a request
+	go func() {
+		select {
+		case rpc := <-rpcCh:
+			// Verify the command
+			req := rpc.Command.(*raft.RequestVoteRequest)
+			if !reflect.DeepEqual(req, &args) {
+				t.Fatalf("command mismatch: %#v %#v", *req, args)
+			}
+
+			rpc.Respond(&resp, nil)
+
+		case <-time.After(200 * time.Millisecond):
+			t.Fatalf("timeout")
+		}
+	}()
+
+	// Transport 2 makes outbound request
+	trans2, err := NewNATSTransportWithLogger("b", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans2.Close()
+
+	var out raft.RequestVoteResponse
+	if err := trans2.RequestVote(trans1.LocalAddr(), &args, &out); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Verify the response
+	if !reflect.DeepEqual(resp, out) {
+		t.Fatalf("command mismatch: %#v %#v", resp, out)
+	}
+}
+
+func TestNATSTransportInstallSnapshot(t *testing.T) {
+	s := RunDefaultServer()
+	defer s.Shutdown()
+	nc := NewDefaultConnection(t)
+	defer nc.Close()
+
+	// Transport 1 is consumer
+	trans1, err := NewNATSTransportWithLogger("a", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans1.Close()
+	rpcCh := trans1.Consumer()
+
+	// Make the RPC request
+	args := raft.InstallSnapshotRequest{
+		Term:         10,
+		Leader:       []byte("kyle"),
+		LastLogIndex: 100,
+		LastLogTerm:  9,
+		Peers:        []byte("blah blah"),
+		Size:         10,
+	}
+	resp := raft.InstallSnapshotResponse{
+		Term:    10,
+		Success: true,
+	}
+
+	// Listen for a request
+	go func() {
+		select {
+		case rpc := <-rpcCh:
+			// Verify the command
+			req := rpc.Command.(*raft.InstallSnapshotRequest)
+			if !reflect.DeepEqual(req, &args) {
+				t.Fatalf("command mismatch: %#v %#v", *req, args)
+			}
+
+			// Try to read the bytes
+			buf := make([]byte, 10)
+			rpc.Reader.Read(buf)
+
+			// Compare
+			if bytes.Compare(buf, []byte("0123456789")) != 0 {
+				t.Fatalf("bad buf %v", buf)
+			}
+
+			rpc.Respond(&resp, nil)
+
+		case <-time.After(200 * time.Millisecond):
+			t.Fatalf("timeout")
+		}
+	}()
+
+	// Transport 2 makes outbound request
+	trans2, err := NewNATSTransportWithLogger("b", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans2.Close()
+
+	// Create a buffer
+	buf := bytes.NewBuffer([]byte("0123456789"))
+
+	var out raft.InstallSnapshotResponse
+	if err := trans2.InstallSnapshot(trans1.LocalAddr(), &args, &out, buf); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Verify the response
+	if !reflect.DeepEqual(resp, out) {
+		t.Fatalf("command mismatch: %#v %#v", resp, out)
+	}
+}
+
+func TestNATSTransportEncodeDecode(t *testing.T) {
+	s := RunDefaultServer()
+	defer s.Shutdown()
+	nc := NewDefaultConnection(t)
+	defer nc.Close()
+
+	// Transport 1 is consumer
+	trans1, err := NewNATSTransportWithLogger("a", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans1.Close()
+
+	local := trans1.LocalAddr()
+	enc := trans1.EncodePeer(local)
+	dec := trans1.DecodePeer(enc)
+
+	if dec != local {
+		t.Fatalf("enc/dec fail: %v %v", dec, local)
+	}
+}
+
+func TestNATSTransportPooledConn(t *testing.T) {
+	s := RunDefaultServer()
+	defer s.Shutdown()
+	nc := NewDefaultConnection(t)
+	defer nc.Close()
+
+	// Transport 1 is consumer
+	trans1, err := NewNATSTransportWithLogger("a", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans1.Close()
+	rpcCh := trans1.Consumer()
+
+	// Make the RPC request
+	args := raft.AppendEntriesRequest{
+		Term:         10,
+		Leader:       []byte("cartman"),
+		PrevLogEntry: 100,
+		PrevLogTerm:  4,
+		Entries: []*raft.Log{
+			&raft.Log{
+				Index: 101,
+				Term:  4,
+				Type:  raft.LogNoop,
+			},
+		},
+		LeaderCommitIndex: 90,
+	}
+	resp := raft.AppendEntriesResponse{
+		Term:    4,
+		LastLog: 90,
+		Success: true,
+	}
+
+	// Listen for a request
+	go func() {
+		for {
+			select {
+			case rpc := <-rpcCh:
+				// Verify the command
+				req := rpc.Command.(*raft.AppendEntriesRequest)
+				if !reflect.DeepEqual(req, &args) {
+					t.Fatalf("command mismatch: %#v %#v", *req, args)
+				}
+				rpc.Respond(&resp, nil)
+
+			case <-time.After(200 * time.Millisecond):
+				return
+			}
+		}
+	}()
+
+	// Transport 2 makes outbound request, 3 conn pool
+	trans2, err := NewNATSTransportWithLogger("b", nc, time.Second, newTestLogger(t))
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer trans2.Close()
+
+	// Create wait group
+	wg := &sync.WaitGroup{}
+	wg.Add(5)
+
+	appendFunc := func() {
+		defer wg.Done()
+		var out raft.AppendEntriesResponse
+		if err := trans2.AppendEntries(trans1.LocalAddr(), &args, &out); err != nil {
+			t.Fatalf("err: %v", err)
+		}
+
+		// Verify the response
+		if !reflect.DeepEqual(resp, out) {
+			t.Fatalf("command mismatch: %#v %#v", resp, out)
+		}
+	}
+
+	// Try to do parallel appends, should stress the conn pool
+	for i := 0; i < 5; i++ {
+		go appendFunc()
+	}
+
+	// Wait for the routines to finish
+	wg.Wait()
+}
